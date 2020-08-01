c6b43a1ae537d0c84da8763eed83ed39a2ef266d
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-08-01 15:10:44 +0900

    Initial Commit

---
+++ Swift5WKWebView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKWebView1
+//
+//  Created by 山岡巧 on 2020/08/01.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
